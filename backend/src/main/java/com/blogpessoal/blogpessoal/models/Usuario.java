bc9c210e16a31f7fe64d482b344d4c3a628a6058
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-05-16 10:16:06 -0300

    adicionando swagger doc - 16/05

--- backend/src/main/java/com/blogpessoal/blogpessoal/models/Usuario.java
+++ backend/src/main/java/com/blogpessoal/blogpessoal/models/Usuario.java
@@ -9,10 +9,13 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @Entity
 @Table(name = "tb_usuarios")
@@ -26,8 +29,10 @@ public class Usuario {
 	@Size(min = 3, max = 255)
 	private String nome;
 
+	@Schema(example = "email@email.com.br")
 	@NotBlank
 	@Size(min = 3, max = 255)
+	@Email
 	private String usuario;
 
 	@NotBlank
