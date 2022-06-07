3df954a2e6d1fcfe5c7295af0be82476906f1e62
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-06-07 09:44:04 -0300

    add cors

--- backend/src/main/java/com/blogpessoal/blogpessoal/controllers/PostagemController.java
+++ backend/src/main/java/com/blogpessoal/blogpessoal/controllers/PostagemController.java
@@ -7,6 +7,8 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +24,7 @@ import com.blogpessoal.blogpessoal.services.PostagemService;
 
 @RestController
 @RequestMapping(value = "/postagens")
+@CrossOrigin(origins = "*",allowedHeaders="*")
 public class PostagemController {
 
 	@Autowired
